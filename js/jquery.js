d10adbe6d17453e91e3415ab4be62868f6d92223
whitedeath820 <allensam0@gmail.com>
2015-04-15 19:10:05 -0600

    Fixed some mistakes but still doesn't work

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,16 @@
 //jquery for the animations
 
 $(function() {
+    
+    //varibles for animation names
+    var AShake = 'animated shake';
+    //end animation listening event
+    var AEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    
+    //animations doesn't work yet but follows the same pattern for every animation note the first one is for testing
     $('.links').on('click',function() {
-        $('p[name=test]').addClass('animated shake');
-    ));
+        $('p[name=test]').addClass(AShake).one(AEnd,function() {
+            $(this).removeClass(AShake);
+        });
+    });
+});
