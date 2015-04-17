730831294c55bb6fcf4f4c106e5c5713f944de30
whitedeath820 <allensam0@gmail.com>
2015-04-17 08:33:30 -0600

    animation working

--- js/jquery.js
+++ js/jquery.js
@@ -1,16 +1,21 @@
 //jquery for the animations
 
 $(function () {
-
     //varibles for animation names
     var AShake = 'animated shake';
+    var AFadein = 'animated fadeIn';
     //end animation listening event
     var AEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
 
-    //animations doesn't work yet but follows the same pattern for every animation note the first one is for testing
-    $('#test').on('click', function () {
-        $('p[name=test]').addClass(AShake).one(AEnd, function () {
-            $(this).removeClass(AShake);
+    //About text fade in-- works
+    $('#linkabout').on('click', function () {
+        $('p[name=pabout]').addClass(AFadein).one(AEnd, function () {
+            $(this).removeClass(AFadein);
+        });
+    });
+    $('#linkabout').on('click', function () {
+        $('h1[name=habout]').addClass(AFadein).one(AEnd, function () {
+            $(this).removeClass(AFadein);
         });
     });
 });
