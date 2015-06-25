79afd0c01ea6c596fd973e7cb844ad87648319c7
pricemc <price.mc1x@gmail.com>
2015-06-24 18:57:10 -0600

    www

---
+++ www/js/controllers.js
@@ -0,0 +1,155 @@
+angular.module('starter.controllers', [])
+
+.controller('UserThreadChatCtrl', function ($scope) {
+
+})
+
+.controller('UserThreadCtrl', function ($scope, $stateParams) {
+
+})
+
+.controller('NearbyThreadCtrl', function ($scope, NearbyThreadsGetter, AuthTokenFactory, UserFactory) {
+
+    $scope.nearbyRefresh = function () {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log("latitude from geolocation: ", position.coords.latitude)
+            console.log("Longitude from geolocation: ", position.coords.longitude)
+
+            NearbyThreadsGetter.nearbyRefresh({
+                    "latitude": position.coords.latitude,
+                    "longitude": position.coords.longitude
+                })
+                .then(function (response) {
+                    $scope.posts = response;
+                }).finally(function () {
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+
+        }, function (error) {
+            alert(error);
+        });
+    };
+
+    $scope.nearbyPost = function () {
+        if (AuthTokenFactory.getToken()) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                UserFactory.getUser().then(function success (response) {
+                    console.log("latitude: ", position.coords.latitude);
+                    console.log("longitude: ", position.coords.longitude);
+                    console.log("content: ", $scope.post.content);
+                    console.log("author: ", response.data.user)
+
+                    NearbyThreadsGetter.nearbyPost({
+                        "post": {
+                            "latitude": position.coords.latitude,
+                            "longitude": position.coords.longitude,
+                            "content": $scope.post.content,
+                            "author": response.data.user
+                        },
+                        "token": AuthTokenFactory.getToken()
+                    }, function(post){
+                        post.post.timestamp = Date.now();
+                        $scope.posts.splice(0,0,post.post);
+                    });
+
+                    $scope.post.content = null;
+                })
+            }, function (error) {
+                alert(error);
+            });
+        } else {
+            alert("You are not signed in. Posting requires that you sign in.");
+        }
+    }
+    
+    $scope.vote = function(postid, dirCondition){
+        console.log("Vote function active on post: " + postid);
+        if(dirCondition){
+            console.log("Upvoting");
+            NearbyThreadsGetter.upvote(postid);
+        }else{
+            console.log("Downvoting");
+            NearbyThreadsGetter.downvote(postid);
+        }
+       
+    }
+})
+
+.controller('AccountCtrl', function ($scope, UserFactory) {
+
+    $scope.hideAllSignins = false;
+    $scope.showSignupForm = false;
+    $scope.showLoginForm = false;
+    $scope.showFBLogin = false;
+    $scope.showInfo = false;
+
+    // initialization
+    UserFactory.getUser().then(function success(response) {
+        $scope.user = response.data;
+    });
+
+    $scope.toggleSignupForm = function () {
+        $scope.showSignupForm = !($scope.showSignupForm);
+        $scope.showLoginForm = false;
+        $scope.showFBLogin = false;
+    }
+
+    $scope.toggleLoginForm = function () {
+        $scope.showLoginForm = !($scope.showLoginForm);
+        $scope.showSignupForm = false;
+        $scope.showFBLogin = false;
+    }
+
+    $scope.toggleFBLogin = function () {
+        $scope.showFBLogin = !($scope.showFBLogin);
+        $scope.showSignupForm = false;
+        $scope.showLoginForm = false;
+    }
+
+    $scope.toggleInfo = function () {
+        if($scope.showInfo) {
+            $scope.showInfo = false;
+        }else {
+            $scope.showInfo = true;
+        }
+    }
+
+    $scope.signup = function (usn, age, em, pwd) {
+        UserFactory.signup(usn, age, em, pwd).then(function success(response) {
+            $scope.signup.usn = null;
+            $scope.signup.age = null;
+            $scope.signup.em = null;
+            $scope.signup.pwd = null;
+            console.log(response);
+            $scope.login(em, pwd);
+        }, function handleError(response) {
+            alert('Error: ' + response.data);
+        });
+    }
+
+    $scope.login = function (em, pwd) {
+        UserFactory.login(em, pwd).then(function success(response) {
+            UserFactory.getUser().then(function success(response) {
+                $scope.user = response.data.user;
+                console.log(response.data);
+            });
+            $scope.login.em = null;
+            $scope.login.pwd = null;
+            $scope.hideAllSignins = true;
+        }, function handleError(response) {
+            alert('Error: ' + response.data);
+        });
+    }
+
+    $scope.FBLogin = function (em, pwd) {
+
+    }
+
+    $scope.logout = function () {
+        UserFactory.logout();
+        $scope.user = null;
+        $scope.username = null;
+        $scope.hideAllSignins = false;
+    }
+
+});
---
+++ www/js/services.js
@@ -0,0 +1,132 @@
+'use strict';
+
+angular.module('starter.services', [], function config($httpProvider) {
+    $httpProvider.interceptors.push('AuthInterceptor');
+})
+
+.constant('API_URL', 'http://52.10.238.99:8080')
+
+.factory('UserFactory', function UserFactory($http, API_URL, AuthTokenFactory, $q) {
+    return {
+        login: login,
+        logout: logout,
+        getUser: getUser,
+        signup: signup
+    };
+
+    function login(email, password) {
+        return $http.post(API_URL + '/login', {
+            email: email,
+            password: password
+        }).then(function success(response) {
+            AuthTokenFactory.setToken(response.data.token);
+            return response;
+        });
+    }
+
+    function logout() {
+        AuthTokenFactory.setToken();
+    }
+
+    function getUser() {
+        if (AuthTokenFactory.getToken()) {
+            return $http.post(API_URL + '/me', {
+                token: AuthTokenFactory.getToken()
+            });
+        } else {
+            return $q.reject({
+                data: 'client has no auth token'
+            });
+        }
+    }
+
+    function signup(s_name, s_age, s_email, s_pwd) {
+        return $http.post(API_URL + '/newUser', {
+            "email": s_email,
+            "password": s_pwd,
+            "username": s_name,
+            "age": s_age
+        }).then(function success(response) {
+            return response;
+        });
+    }
+})
+
+.factory('AuthTokenFactory', function AuthTokenFactory($window) {
+        var store = $window.localStorage;
+        var key = 'auth-token';
+        return {
+            getToken: getToken,
+            setToken: setToken
+        };
+
+        function getToken() {
+            return store.getItem(key);
+        }
+
+        function setToken(token) {
+            if (token) {
+                store.setItem(key, token);
+            } else {
+                store.removeItem(key);
+            }
+        }
+    })
+    .factory('AuthInterceptor', function AuthInterceptor(AuthTokenFactory) {
+        return {
+            request: addToken
+        };
+
+        function addToken(config) {
+            var token = AuthTokenFactory.getToken();
+            if (token) {
+                config.headers = config.headers || {};
+                config.headers.Authorization = 'Bearer ' + token;
+            }
+            return config;
+        }
+    })
+
+.factory('NearbyThreadsGetter', function NearbyThradsGetter($http, API_URL) {
+    var factory = {
+        nearbyRefresh: nearbyRefresh,
+        nearbyPost: nearbyPost,
+        upvote: upvote,
+        downvote: downvote
+    };
+
+    factory.getNearby = function (location) {
+        return $http.post(API_URL + '/nearbyRO', location)
+            .then(function success(response) {
+                console.log("Returned data:\n" + response);
+                console.dir(response);
+                return response.data;
+            });
+    }
+
+    function upvote(id) {
+        return $http.post(API_URL + '/upvote', {headers: 'application/json', data: id}).then(function success(response) {
+            console.log("Upvote Successful")
+        });
+    }
+
+    function downvote(id) {
+        return $http.post(API_URL + '/downvote', {headers: 'application/json', data: id}).then(function success(response) {
+            console.log("Downvote Successful")
+        });
+    }
+
+    function nearbyPost(post, cb) {
+        return $http.post(API_URL + '/nearbyPost', post)
+            .then(function success(response) {
+                cb(post);
+                console.log("Comment has been posted");
+            });
+    }
+
+    function nearbyRefresh(location) {
+        return factory.getNearby(location);
+    }
+
+    return factory;
+});
