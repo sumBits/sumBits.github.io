ff67bf6a743e12b5b51ce4e7b136d7022cc6abfb
WhiteDeath820 <allensam0@gmail.com>
2015-10-28 11:40:37 -0600

    toggle almost working i think

--- js/controllers.js
+++ js/controllers.js
@@ -1,15 +1,28 @@
 webctrl = angular.module('app.controllers', []);
 
 webctrl.controller('homeCtrl', function ($scope) {
-  $scope.port = function (array_number) {
-
+  $scope.port = function (n) {
+    $scope.newOuter = false;
+    $scope.newOuterHide = true;
+    var last_state = []
+    last_state.push(n);
+    switch (n) {
+      case 0:
+        if (last_state[0] === last_state[1]) {
+          $scope.newOuterHide = true;
+          $scope.newOuter = false;
+        } else {
+          $scope.newOuterHide = false;
+          $scope.newOuter = true;
+        }
+        last_state.shift();
+        break;
+    }
     var info = [{
       name: 'Sam Allen',
       description: 'heyyyy'
     }]
 
-    $scope.product = info[array_number]
-
-  $scope.newOuter = true;
+    $scope.product = info[n]
   }
 });
