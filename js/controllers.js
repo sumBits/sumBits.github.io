8473b4ff5f42019b8280f4a5679c9fa4d3025c55
WhiteDeath820 <allensam0@gmail.com>
2015-11-01 22:15:51 -0700

    debugging

--- js/controllers.js
+++ js/controllers.js
@@ -1,33 +1,33 @@
 webctrl = angular.module('app.controllers', []);
 
 webctrl.controller('homeCtrl', function ($scope) {
-  $scope.products = info;
   var info = [{
-    namefirst: 'Sam',
-    namelast: 'Allen',
-    picture: 'img/sam.jpg',
-    description: 'please work'
+      namefirst: 'Sam',
+      namelast: 'Allen',
+      picture: 'img/sam.jpg',
+      description: 'please work'
   }, {
-    namefirst: 'Jordan',
-    namelast: 'Jones',
-    picture: 'img/jordan.jpg',
-    description: 'hbak; lknbasn;kldsvjfbk;n;'
+      namefirst: 'Jordan',
+      namelast: 'Jones',
+      picture: 'img/jordan.jpg',
+      description: 'hbak; lknbasn;kldsvjfbk;n;'
   }, {
-    namefirst: 'Henry',
-    namelast: 'Kaufman',
-    picture: 'img/placeholder.jpg',
-    description: 'hey'
+      namefirst: 'Henry',
+      namelast: 'Kaufman',
+      picture: 'img/placeholder.jpg',
+      description: 'hey'
   }, {
-    namefirst: 'Matthew',
-    namelast: 'Price',
-    picture: 'img/placeholder.jpg',
-    description: 'hedav'
+      namefirst: 'Matthew',
+      namelast: 'Price',
+      picture: 'img/placeholder.jpg',
+      description: 'hedav'
   }, {
-    namefirst: 'Daniel',
-    namelast: 'Zamosh',
-    picture: 'img/daniel.jpg',
-    description: 'heyfvabbasfrdf'
+      namefirst: 'Daniel',
+      namelast: 'Zamosh',
+      picture: 'img/daniel.jpg',
+      description: 'heyfvabbasfrdf'
   }];
 
+  $scope.products = info;
 
 });
