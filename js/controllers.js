fbcbfb83fcf929f751cf963a5455b47f440e70e9
WhiteDeath820 <allensam0@gmail.com>
2015-11-01 22:53:41 -0700

    added librarys to code instead of links

--- js/controllers.js
+++ js/controllers.js
@@ -1,33 +1,30 @@
 webctrl = angular.module('app.controllers', []);
-
+var info = [{
+    namefirst: 'Sam',
+    namelast: 'Allen',
+    picture: 'img/sam.jpg',
+    description: 'please work'
+}, {
+    namefirst: 'Jordan',
+    namelast: 'Jones',
+    picture: 'img/jordan.jpg',
+    description: 'hbak; lknbasn;kldsvjfbk;n;'
+}, {
+    namefirst: 'Henry',
+    namelast: 'Kaufman',
+    picture: 'img/placeholder.jpg',
+    description: 'hey'
+}, {
+    namefirst: 'Matthew',
+    namelast: 'Price',
+    picture: 'img/placeholder.jpg',
+    description: 'hedav'
+}, {
+    namefirst: 'Daniel',
+    namelast: 'Zamosh',
+    picture: 'img/daniel.jpg',
+    description: 'heyfvabbasfrdf'
+}];
 webctrl.controller('homeCtrl', function ($scope) {
-  var info = [{
-      namefirst: 'Sam',
-      namelast: 'Allen',
-      picture: 'img/sam.jpg',
-      description: 'please work'
-  }, {
-      namefirst: 'Jordan',
-      namelast: 'Jones',
-      picture: 'img/jordan.jpg',
-      description: 'hbak; lknbasn;kldsvjfbk;n;'
-  }, {
-      namefirst: 'Henry',
-      namelast: 'Kaufman',
-      picture: 'img/placeholder.jpg',
-      description: 'hey'
-  }, {
-      namefirst: 'Matthew',
-      namelast: 'Price',
-      picture: 'img/placeholder.jpg',
-      description: 'hedav'
-  }, {
-      namefirst: 'Daniel',
-      namelast: 'Zamosh',
-      picture: 'img/daniel.jpg',
-      description: 'heyfvabbasfrdf'
-  }];
-
   $scope.products = info;
-
 });
