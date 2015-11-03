e6312664c6d511b6faf828a28b9a1e509568d3bf
WhiteDeath820 <allensam0@gmail.com>
2015-11-03 15:00:10 -0700

    finally

--- js/controllers.js
+++ js/controllers.js
@@ -1,30 +1,61 @@
 webctrl = angular.module('app.controllers', []);
-var info = [{
-    namefirst: 'Sam',
-    namelast: 'Allen',
-    picture: 'img/sam.jpg',
-    description: 'please work'
-}, {
-    namefirst: 'Jordan',
-    namelast: 'Jones',
-    picture: 'img/jordan.jpg',
-    description: 'hbak; lknbasn;kldsvjfbk;n;'
-}, {
-    namefirst: 'Henry',
-    namelast: 'Kaufman',
-    picture: 'img/placeholder.jpg',
-    description: 'hey'
-}, {
-    namefirst: 'Matthew',
-    namelast: 'Price',
-    picture: 'img/placeholder.jpg',
-    description: 'hedav'
-}, {
-    namefirst: 'Daniel',
-    namelast: 'Zamosh',
-    picture: 'img/daniel.jpg',
-    description: 'heyfvabbasfrdf'
-}];
+
 webctrl.controller('homeCtrl', function ($scope) {
-  $scope.products = info;
+  $scope.hideinit = false;
+  $scope.saminfo = false;
+  $scope.henryinfo = false;
+  $scope.jordaninfo = false;
+  $scope.mattinfo = false;
+  $scope.daninfo = false;
+  $scope.mouseover = function (n) {
+    switch (n) {
+      case 0:
+        $scope.hideinit = true;
+        $scope.henryinfo = false;
+        $scope.jordaninfo = false;
+        $scope.mattinfo = false;
+        $scope.daninfo = false;
+        $scope.saminfo = true;
+        break;
+      case 1:
+        $scope.hideinit = true;
+        $scope.saminfo = false;
+        $scope.jordaninfo = false;
+        $scope.mattinfo = false;
+        $scope.daninfo = false;
+        $scope.henryinfo = true;
+        break;
+      case 2:
+        $scope.hideinit = true;
+        $scope.saminfo = false;
+        $scope.henryinfo = false;
+        $scope.mattinfo = false;
+        $scope.daninfo = false;
+        $scope.jordaninfo = true;
+        break;
+      case 3:
+        $scope.hideinit = true;
+        $scope.saminfo = false;
+        $scope.henryinfo = false;
+        $scope.jordaninfo = false;
+        $scope.daninfo = false;
+        $scope.mattinfo = true;
+      case 4:
+        $scope.hideinit = true;
+        $scope.saminfo = false;
+        $scope.henryinfo = false;
+        $scope.jordaninfo = false;
+        $scope.mattinfo = false;
+        $scope.daninfo = true;
+        break;
+    }
+  }
+  $scope.mouseover_reset = function () {
+    $scope.hideinit = false;
+    $scope.saminfo = false;
+    $scope.henryinfo = false;
+    $scope.jordaninfo = false;
+    $scope.mattinfo = false;
+    $scope.daninfo = false;
+  }
 });
